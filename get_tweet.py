e0f6bfcc022a6c283f5e26aa2b8982fb1d3172aa
Peter Gao <pgao@pinterest.com>
2013-07-23 00:21:59 -0700

    fix alignment, tech debt

--- get_tweet.py
+++ get_tweet.py
@@ -3,20 +3,21 @@ sys.path.insert(0, 'static/twitter/')
 
 import twitter
 
-api = twitter.Api(consumer_key="ElptdK9AGjxOBuORbiuTQ", consumer_secret="iufJBk1qhX84VVeCeeqeOJrVgDkqmtiVobNTpMKUA", 
-    access_token_key="370357185-yFfM8KRTOLuQJqfaURThsTN53clQK5oxAyTbvmb0", access_token_secret="1nSEOq2a6STHJm4gIVObMI0bkoL6cG8zH7n0qQ2bKc")
+api = twitter.Api(consumer_key="ElptdK9AGjxOBuORbiuTQ",
+                  consumer_secret="iufJBk1qhX84VVeCeeqeOJrVgDkqmtiVobNTpMKUA",
+                  access_token_key="370357185-yFfM8KRTOLuQJqfaURThsTN53clQK5oxAyTbvmb0",
+                  access_token_secret="1nSEOq2a6STHJm4gIVObMI0bkoL6cG8zH7n0qQ2bKc")
 
-#to use this, type "sudo pip install twitter" first
 
 def get_tweets(keyword="e"):
-	if keyword == " ":
-		keyword = "e"
-	statuses = api.GetSearch(keyword)
-	text = []
-	for s in statuses:
-		text.append(s.GetText())
-		#print s.GetText()
-	return text
+    if keyword == " ":
+        keyword = "e"
+    statuses = api.GetSearch(keyword)
+    text = []
+    for s in statuses:
+        text.append(s.GetText())
+        #print s.GetText()
+    return text
 
 if __name__ == "__main__":
     get_tweets()
--- server.py
+++ server.py
@@ -8,7 +8,6 @@ import simplejson
 import classify
 import get_tweet
 import os
-import random
 import sys
 sys.path.insert(0, 'flot/')
 
@@ -18,26 +17,54 @@ classifier = classify.NaiveBayes()
 
 tweetbuffer = {}
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
+
 
 class JQueryHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("/flot/jquery.js")
 
+
 class FlotHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("/flot/jquery.flot.js")
+
 
 class StyleHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("style.css")
 
+
+def get_stock_quotes(name):
+    #f = urllib2.urlopen("http://download.finance.yahoo.com/d/quotes.csv?s=^IXIC&f=l1n ")
+    f = urllib2.urlopen("http://download.finance.yahoo.com/d/quotes.csv?s=" + name.upper() + "&f=l1n")
+    #quotes = f.read().split("\r\n")
+    quotes = f.read()
+    quotationMark = False
+    for i, char in enumerate(quotes):
+        if char == '"':
+            quotationMark = not quotationMark
+        if char == "," and quotationMark:
+            quotes = quotes[:i] + quotes[i + 1:]
+    quotes = quotes.replace("\r\n", "").replace('"', '').split(",")
+    return quotes
+
+
+def get_tweet_sentiments(name):
+    tweets = get_tweet.get_tweets(name)
+    sentiments = []
+    for tweet in tweets:
+        sentiments.append((tweet, classifier.classify(tweet)))
+    return sentiments
+
+
 class DataHandler(tornado.web.RequestHandler):
     def get(self):
         name = str(self.get_argument("name", "^IXIC"))
-        quotes = self.get_stock_quotes(name)
+        quotes = get_stock_quotes(name)
         if quotes[-1] == "NASDAQComposite":
             quotes[-1] = "NASDAQ"
         elif quotes[-1] == "S&P 500":
@@ -46,7 +73,7 @@ class DataHandler(tornado.web.RequestHandler):
         global tweetbuffer
         if name not in tweetbuffer or not tweetbuffer[name]:
             print name + " not in tweetbuffer"
-            sentiments = self.get_tweet_sentiments(quotes[-1].split()[0])
+            sentiments = get_tweet_sentiments(quotes[-1].split()[0])
             #sentiments = self.get_tweet_sentiments(name)
             tweetbuffer[name] = []
             tweetbuffer[name] = sentiments
@@ -58,25 +85,6 @@ class DataHandler(tornado.web.RequestHandler):
         self.set_header("Content-Type", "application/json")
         print [quotes, tweet]
         return self.write(simplejson.dumps([quotes, tweet]))
-    def get_stock_quotes(self, name):
-        #f = urllib2.urlopen("http://download.finance.yahoo.com/d/quotes.csv?s=^IXIC&f=l1n ")
-        f = urllib2.urlopen("http://download.finance.yahoo.com/d/quotes.csv?s=" + name.upper() + "&f=l1n")
-        #quotes = f.read().split("\r\n")
-        quotes = f.read()
-        quotationMark = False
-        for i, char in enumerate(quotes):
-            if char == '"':
-                quotationMark = not quotationMark
-            if char == "," and quotationMark:
-                quotes = quotes[:i] + quotes[i + 1:]
-        quotes = quotes.replace("\r\n", "").replace('"', '').split(",")
-        return quotes
-    def get_tweet_sentiments(self, name):
-        tweets = get_tweet.get_tweets(name)
-        sentiments = []
-        for tweet in tweets:
-            sentiments.append((tweet, classifier.classify(tweet)))
-        return sentiments
 
 application = tornado.web.Application([
     (r"/", MainHandler),
@@ -84,7 +92,7 @@ application = tornado.web.Application([
     #(r"/jQuery.js", tornado.web.StaticFileHandler, {'path': '/flot/'}),
     #(r"/jQuery.flot.js", tornado.web.StaticFileHandler, {'path': '/flot/jQuery.flot.js'}),
     (r"/style.css", StyleHandler)
-], debug=True, static_path=os.path.join(os.path.dirname(__file__) , 'static'))
+], debug=True, static_path=os.path.join(os.path.dirname(__file__), 'static'))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
